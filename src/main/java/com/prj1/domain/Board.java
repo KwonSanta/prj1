4104ba083535ef5ddd6c4776a9b1fd5c83953c73
KwonSanta <KwonSanta@gmail.com>
2024-05-01 11:29:23 +0900

    게시물 목록보기 완성

---
+++ src/main/java/com/prj1/domain/Board.java
@@ -0,0 +1,15 @@
+package com.prj1.domain;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+public class Board {
+    private Integer id;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDateTime inserted;
+}
